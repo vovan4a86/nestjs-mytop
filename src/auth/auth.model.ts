b52d2833ccffdc2cf416d8852cedfe4d03a247a9
amer1can <amer1can@mail.ru>
2022-04-10 16:47:25 +0300

    model connected

--- src/auth/auth.model.ts
+++ src/auth/auth.model.ts
@@ -1,4 +1,14 @@
-export class AuthModel {
+import { prop } from "@typegoose/typegoose";
+import { TimeStamps, Base } from "@typegoose/typegoose/lib/defaultClasses";
+
+//наследуем Base через interface т.к. нельзя расширять одновременно от двух классов (TimeStamp и Base)
+//TimeStamp добавит поля createdAt, updatedAt: Date
+//Base - _id
+export interface AuthModel extends Base {}
+export class AuthModel extends TimeStamps {
+  @prop({ unique: true }) //поле уникальное и является индексом (index: true)
   email: string
+
+  @prop()
   passwordHash: string
 }
--- src/main-page/main-page.module.ts
+++ src/main-page/main-page.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { MainPageController } from './main-page.controller';
+import { TypegooseModule } from "nestjs-typegoose";
+import { MainPageModel } from "./main-page.model";
 
 @Module({
-  controllers: [MainPageController]
+  controllers: [MainPageController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: MainPageModel,
+        schemaOptions: {
+          collection: 'MainPage' //если не указать коллекцию default: MainPageModel
+        }
+      }
+    ])
+  ]
 })
 export class MainPageModule {}
